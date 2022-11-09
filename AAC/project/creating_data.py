6b1e46433937804d16cb3b43654ec71182ebb82f
RamiBouk <ramiboukaroura@gmail.com>
2022-11-09 12:06:31 +0100

    worked on aac project

--- AAC/project/creating_data.py
+++ AAC/project/creating_data.py
@@ -5,27 +5,28 @@ from math import floor
 import matplotlib.pyplot as plt
 import time
 import sys
-import json
 
-def algo(sort_name,arr):
-    if sort_name=="bubble":
+
+def algo(sort_name, arr):
+    if sort_name == "bubble":
         sf.bubble_sort(arr)
-    if sort_name=="new_quick":
+    if sort_name == "new_quick":
         sf.quick_sort(arr)
-    if sort_name=="merge":
+    if sort_name == "merge":
         sf.merge_sort(arr)
-    if sort_name=="insertion":
+    if sort_name == "insertion":
         sf.insertion_sort(arr)
-    if sort_name=="heap":
+    if sort_name == "heap":
         sf.heap_sort(arr)
-    if sort_name=="selection":
+    if sort_name == "selection":
         sf.selection_sort(arr)
 
 
-MAX_ARRAY_SIZE=7000
-MAX_NUMBER=1_000
-NUMBER_OF_SAMPLES=5
-STEP=10
+MAX_ARRAY_SIZE = 7000
+MAX_NUMBER = 1_000
+NUMBER_OF_SAMPLES = 5
+STEP = 10
+
 
 def study(max_array_size=MAX_ARRAY_SIZE,
           min_array_size=0,
@@ -35,39 +36,41 @@ def study(max_array_size=MAX_ARRAY_SIZE,
           algo_name="bubble",
           array_sizes=[],
           durations=[],
-                 ):
-    execution_time=0
-    for array_size in range(max_array_size,min_array_size,-step):
-        for i in range(number_or_samples,0,-1):
-            start=time.time()
-            print(f' {floor((max_array_size-array_size)*100/max_array_size)}%'+
-                  f' ETA:{number_or_samples*array_size*execution_time/step}s'
-                  ,end='\r')
+          ):
+    execution_time = 0
+    for array_size in range(max_array_size, min_array_size, -step):
+        for i in range(number_or_samples, 0, -1):
+            start = time.time()
+            print(f' {floor((max_array_size-array_size)*100/max_array_size)}%' +
+                  f' ETA:{number_or_samples*array_size*execution_time/step}s', end='\r')
 
-            arr=[floor(max_number*random()) for _ in range(array_size)]
-            algo(algo_name,arr)
-            end=time.time()
-            execution_time=end-start
+            arr = [floor(max_number*random()) for _ in range(array_size)]
+            algo(algo_name, arr)
+            end = time.time()
+            execution_time = end-start
             array_sizes.append(array_size)
             durations.append(execution_time)
 
+
 def main():
     sys.setrecursionlimit(50000)
-    times={}
-    arr_sizes=[]
-    durations=[]
-    algo_name="insertion"
+    times = {}
+    arr_sizes = []
+    durations = []
+    algo_name = "insertion"
     study(array_sizes=arr_sizes,
           durations=durations,
           min_array_size=6000,
           algo_name=algo_name,
-              )
-    times["array sizes"]=arr_sizes
-    times["durations"]=durations
+          )
+    times["array sizes"] = arr_sizes
+    times["durations"] = durations
     df = pd.DataFrame(times)
     print(df)
-    df.to_csv(f"{algo_name}.csv",mode="a" )
-    df.plot(kind="scatter",x="array sizes",y="durations")
+    df.to_csv(f"{algo_name}.csv", mode="a")
+    df.plot(kind="scatter", x="array sizes", y="durations")
     plt.show()
+
+
 if __name__ == "__main__":
     main()
--- AAC/project/sorting_functions.py
+++ AAC/project/sorting_functions.py
@@ -1,82 +1,82 @@
 from random import random
 from math import floor
-from math import log2,log
+from math import log2, log
 import sys
-## Functions:
+# Functions:
+
+
 def bubble_sort(array):
-    sorted=False
-    array_size=len(array)
-    while(not sorted):
-        sorted=True
-        for i in range(1,array_size):
-            if(array[i-1]>array[i]):
-                array[i],array[i-1]=array[i-1],array[i]
-                sorted=False
+    sorted = False
+    array_size = len(array)
+    while (not sorted):
+        sorted = True
+        for i in range(1, array_size):
+            if (array[i-1] > array[i]):
+                array[i], array[i-1] = array[i-1], array[i]
+                sorted = False
     return array
+
 
 def merge_sort(array):
-    length=len(array)
-    if(length==1):
+    length = len(array)
+    if (length == 1):
         return array
-    array1=merge_sort(array[:length//2])
-    array2=merge_sort(array[length//2:])
-    i,j=0,0
-    result_array=[]
-    while(len(array1) and len(array2)):
-        if(array1[0]<array2[0]):
-           result_array.append(array1.pop(0))
+    array1 = merge_sort(array[:length//2])
+    array2 = merge_sort(array[length//2:])
+    i, j = 0, 0
+    result_array = []
+    while (len(array1) and len(array2)):
+        if (array1[0] < array2[0]):
+            result_array.append(array1.pop(0))
         else:
-           result_array.append(array2.pop(0))
-    resutls_array=result_array+array1+array2
+            result_array.append(array2.pop(0))
+    resutls_array = result_array+array1+array2
     return resutls_array
 
+
 def quick_sort(array):
-    if(len(array)==1):
+    if (len(array) == 1):
         return array
 
-    poped=array.pop()
-    sorted_array=quick_sort(array)
-    length=len(sorted_array)
-    if(poped>=sorted_array[length-1]):
-        sorted_array.append( poped)
+    poped = array.pop()
+    sorted_array = quick_sort(array)
+    length = len(sorted_array)
+    if (poped >= sorted_array[length-1]):
+        sorted_array.append(poped)
         return sorted_array
-    high,low=length-1,0
-    while(low<high):
-        mid=(high+low)//2
-        if(poped<=sorted_array[mid]):
-            high=mid
+    high, low = length-1, 0
+    while (low < high):
+        mid = (high+low)//2
+        if (poped <= sorted_array[mid]):
+            high = mid
         else:
-            low=mid+1
-    sorted_array.insert(low,poped)
+            low = mid+1
+    sorted_array.insert(low, poped)
     return sorted_array
 
+
 def insertion_sort(array):
-    length=len(array)
-    for i in range(1,length):
-        j=i
-        while(j>0 and array[j-1]>array[j]):
-            array[j-1],array[j]=array[j],array[j-1]
-            j-=1
+    length = len(array)
+    for i in range(1, length):
+        j = i
+        while (j > 0 and array[j-1] > array[j]):
+            array[j-1], array[j] = array[j], array[j-1]
+            j -= 1
     return array
-def heapify(array):
-    for i in range(1,tree_depth):
-        m=2**(i-1)
-        left_tree.append(tree[i][:m])
-        right_tree.append(tree[i][m:])
 
-def heapify(arr,N,i):
-    largest = i
-    l=2*i+1
-    r=2*i+2
-    if l<N and arr[largest]<arr[l]:
-        largest=l
 
-    if r<N and arr[largest]<arr[r]:
-        largest=r
+def heapify(arr, N, root):
+    largest = root
+    left = 2*root+1
+    right = 2*root+2
+    if left < N and arr[largest] < arr[left]:
+        largest = left
+    if right < N and arr[largest] < arr[right]:
+        largest = right
+    if largest != root:
+        arr[root], arr[largest] = arr[largest], arr[root]
+        heapify(arr, N, largest)
 
-    if largest!=i:
-        arr[i],arr[largest]=arr[largest],arr[i]
-        heapify(arr,N,largest)
 
 def heap_sort(arr):
     N = len(arr)
@@ -87,28 +87,32 @@ def heap_sort(arr):
         heapify(arr, i, 0)
     return arr
 
+
 def selection_sort(array):
-    if(len(array)==1):
+    if (len(array) == 1):
         return array
-    min_index=0
+    min_index = 0
     for i in range(len(array)):
-        if(array[i]<array[min_index]):
-            min_index=i
-    min=array.pop(min_index)
-    array=selection_sort(array)
+        if (array[i] < array[min_index]):
+            min_index = i
+    min = array.pop(min_index)
+    array = selection_sort(array)
     array.insert(0, min)
     return array
 
-## Conststants:
-ARRAY_SIZE=100_00
+
+# Conststants:
+ARRAY_SIZE = 100_00
 
 
 def main():
     sys.setrecursionlimit(50000)
-    while(True):
-        array=[floor(10*random()) for i in range(10)]
-        new_array=quick_sort(array.copy())
+    while (True):
+        array = [floor(10*random()) for i in range(10)]
+        new_array = quick_sort(array.copy())
         array.sort()
-        print(new_array==array)
+        print(new_array == array)
+
+
 if __name__ == "__main__":
     main()
